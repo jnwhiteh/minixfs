f07d9e1dd2d6feaadebe9b0f4d861e8fcd1f1a37
James Whitehead II <jnwhiteh@gmail.com>
2011-07-06 19:22:04 +0100

    Add initial implementation of mcheck tool

---
+++ cmd/mcheck/mcheck.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/ast"
+	"log"
+	"rog-go.googlecode.com/hg/exp/go/parser"
+	"rog-go.googlecode.com/hg/exp/go/types"
+)
+
+type astVisitor func(n ast.Node) bool
+
+func (f astVisitor) Visit(n ast.Node) ast.Visitor {
+	if f(n) {
+		return f
+	}
+	return nil
+}
+
+func debug(fmt string) {
+        if *showDebug {
+                log.Print(fmt)
+        }
+}
+
+func debugf(fmt string, args ... interface{}) {
+        if *showDebug {
+                debugf(fmt, args...)
+        }
+}
+
+func checkType(e ast.Expr) {
+	// Fetch the type of this expression
+	obj, objtype := types.ExprType(e, types.DefaultImporter)
+	if e != nil {
+		pos := types.FileSet.Position(e.Pos())
+
+		if obj != nil {
+			debugf("%s: %s %q <%T>", pos, objtype.Kind, objtype.Node, objtype.Node)
+			arr, ok := objtype.Node.(*ast.ArrayType)
+			if ok {
+				debugf("IT IS AN ARRAY TYPE")
+				// Check to see if the type of Elt is a star expression
+				star, ok := arr.Elt.(*ast.StarExpr)
+				if ok {
+					checkType(star.X)
+				}
+			}
+		} else {
+			debugf("%s: Could not determine type %T", pos, e)
+		}
+	}
+}
+
+func checkExprs(pkg *ast.File) {
+	var visit astVisitor
+	visit = func(n ast.Node) bool {
+		if n != nil {
+			debugf("Visiting node at %s of type %T", types.FileSet.Position(n.Pos()), n)
+		}
+
+		switch n := n.(type) {
+		case *ast.KeyValueExpr:
+			// Do not try to resolve
+		case *ast.ArrayType:
+			checkType(n)
+		case ast.Expr:
+			checkType(n)
+		}
+
+		switch n := n.(type) {
+		case *ast.ImportSpec:
+			if n.Name != nil && n.Name.Name == "." {
+				// we don't support this
+				return false
+			}
+			return true
+		case *ast.FuncDecl:
+			// add object for init functions
+			if n.Recv == nil && n.Name.Name == "init" {
+				n.Name.Obj = ast.NewObj(ast.Fun, "init")
+			}
+			return true
+		case *ast.Ident:
+			if n.Name == "_" {
+				return false
+			}
+		case *ast.KeyValueExpr:
+			// don't try to resolve the key part of a key-value
+			// because it might be a map key which doesn't
+			// need resolving, and we can't tell without being
+			// complicated with types.
+			ast.Walk(visit, n.Value)
+			return false
+		case *ast.SelectorExpr:
+			ast.Walk(visit, n.X)
+		case *ast.File:
+			for _, d := range n.Decls {
+				ast.Walk(visit, d)
+			}
+			return false
+		default:
+			return true
+		}
+		return false
+	}
+
+	ast.Walk(visit, pkg)
+}
+
+var typeMatch *string = flag.String("type", "Superblock", "the type for which you want to scan")
+var showDebug *bool = flag.Bool("debug", false, "display debug information")
+
+func main() {
+	flag.Parse()
+	pkgs, _ := parser.ParseFiles(types.FileSet, flag.Args(), 0)
+	for _, pkg := range pkgs {
+		fmt.Printf("Checking package '%s'\n", pkg.Name)
+		for _, f := range pkg.Files {
+			fmt.Printf("Scanning %s\n", types.FileSet.Position(f.Package).Filename)
+			checkExprs(f)
+		}
+	}
+}
---
+++ cmd/mcheck/readme.txt
@@ -0,0 +1,4 @@
+This command requires the following libraries:
+
+goinstall rog-go.googlecode.com/hg/exp/go/types
+goinstall rog-go.googlecode.com/hg/exp/go/parser
