b9b4af039c1ba9c7553ee4ca6f56faf66957f9d0
James Whitehead II <jnwhiteh@gmail.com>
2011-08-18 14:53:22 +0100

    Add messages for FileSystem

--- pkg/minixfs/messages.go
+++ pkg/minixfs/messages.go
@@ -4,7 +4,9 @@ import (
 	"os"
 )
 
-// Device messages
+//////////////////////////////////////////////////////////////////////////////
+// Messages for Device interface
+//////////////////////////////////////////////////////////////////////////////
 
 type m_dev_req struct {
 	call CallNumber
@@ -16,7 +18,9 @@ type m_dev_res struct {
 	err os.Error
 }
 
-// Cache messages
+//////////////////////////////////////////////////////////////////////////////
+// Messages for BlockCache interface
+//////////////////////////////////////////////////////////////////////////////
 
 type m_cache_req interface {
 	is_m_cache_req()
@@ -80,3 +84,56 @@ var _ m_cache_req = m_cache_req_flush{}
 var _ m_cache_res = m_cache_res_err{}
 var _ m_cache_res = m_cache_res_get{}
 var _ m_cache_res = m_cache_res_empty{}
+
+//////////////////////////////////////////////////////////////////////////////
+// Messages for FileSystem
+//////////////////////////////////////////////////////////////////////////////
+
+type m_fs_req interface {
+	is_m_fs_req()
+}
+
+type m_fs_res interface {
+	is_m_fs_res()
+}
+
+// Request messages
+type m_fs_req_close struct {}
+type m_fs_req_mount struct {
+	dev BlockDevice
+	path string
+}
+type m_fs_req_unmount struct {
+	dev BlockDevice
+}
+type m_fs_req_spawn struct {
+	umask uint16
+	rootpath string
+}
+
+// Response messages
+type m_fs_res_err struct {
+	err os.Error
+}
+type m_fs_res_spawn struct {
+	proc *Process
+	err os.Error
+}
+
+// For type-checking
+func (m m_fs_req_close) is_m_fs_req() { }
+func (m m_fs_req_mount) is_m_fs_req() { }
+func (m m_fs_req_unmount) is_m_fs_req() { }
+func (m m_fs_req_spawn) is_m_fs_req() { }
+
+func (m m_fs_res_err) is_m_fs_res() { }
+func (m m_fs_res_spawn) is_m_fs_res() { }
+
+// Check interface implementation
+var _ m_fs_req = m_fs_req_close{}
+var _ m_fs_req = m_fs_req_mount{}
+var _ m_fs_req = m_fs_req_unmount{}
+var _ m_fs_req = m_fs_req_spawn{}
+
+var _ m_fs_res = m_fs_res_err{}
+var _ m_fs_res = m_fs_res_spawn{}
