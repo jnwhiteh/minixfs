4d9db3be8282285268cfe914070d25c342fa61fd
James Whitehead II <jnwhiteh@gmail.com>
2011-09-27 10:27:39 +0100

    Create a new ErrorLevel testutil function

--- pkg/minixfs/testutils/utils.go
+++ pkg/minixfs/testutils/utils.go
@@ -6,10 +6,14 @@ import (
 	"testing"
 )
 
-func ErrorHere(test *testing.T, str string, args ...interface{}) {
-	_, file, line, _ := runtime.Caller(1)
+func ErrorLevel(test *testing.T, level int, str string, args ...interface{}) {
+	_, file, line, _ := runtime.Caller(level)
 	info := fmt.Sprintf("[%s:%d] ", file, line)
 	test.Errorf(info + str, args...)
+}
+
+func ErrorHere(test *testing.T, str string, args ...interface{}) {
+	ErrorLevel(test, 2, str, args...)
 }
 
 func FatalHere(test *testing.T, str string, args ...interface{}) {
