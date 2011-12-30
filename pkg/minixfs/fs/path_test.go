97604094e1cf3f1599c687c125bfba43968965d7
Jim Whitehead II <jnwhiteh@gmail.com>
2011-12-30 14:30:41 +0000

    Add a comment to EatPath test

--- pkg/minixfs/fs/path_test.go
+++ pkg/minixfs/fs/path_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// Test that path lookups function properly
 func TestEatPath(test *testing.T) {
 	fs, err := OpenFileSystemFile("../../../minix3root.img")
 	if err != nil {
