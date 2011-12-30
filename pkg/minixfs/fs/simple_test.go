8aa2ebe909580af32d4320ab70c945e59a84c5e5
Jim Whitehead II <jnwhiteh@gmail.com>
2011-12-30 13:09:28 +0000

    Add a test for shutdown/cleanup

---
+++ pkg/minixfs/fs/simple_test.go
@@ -0,0 +1,18 @@
+package fs
+
+import (
+	. "../../minixfs/testutils/_obj/minixfs/testutils"
+	"testing"
+)
+
+func TestShutdown(test *testing.T) {
+	fs, err := OpenFileSystemFile("../../../minix3root.img")
+	if err != nil {
+		FatalHere(test, "Failed opening file system: %s", err)
+	}
+
+	err = fs.Shutdown()
+	if err != nil {
+		FatalHere(test, "Failed shutting down: %s", err)
+	}
+}
